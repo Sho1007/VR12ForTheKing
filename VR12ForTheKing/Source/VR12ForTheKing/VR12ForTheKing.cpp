673b389590d6f56184f271ea319edcf1b7d47ff3
Sho1007 <sho1007@kookmin.ac.kr>
2023-04-20 19:04:50 +0900

    Add Project / Install LFS

---
+++ VR12ForTheKing/Source/VR12ForTheKing/VR12ForTheKing.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "VR12ForTheKing.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VR12ForTheKing, "VR12ForTheKing" );
