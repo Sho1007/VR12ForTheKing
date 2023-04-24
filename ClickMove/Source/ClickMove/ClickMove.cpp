25bfefa90ae2ac8177c35ed45876204ed98c3fca
Jelly <pbp980822@nate.com>
2023-04-24 16:20:20 +0900

    Create ClickMove Project

---
+++ ClickMove/Source/ClickMove/ClickMove.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ClickMove.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ClickMove, "ClickMove" );
---
+++ ClickMove/Source/ClickMove/ClickMoveGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ClickMoveGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CLICKMOVE_API AClickMoveGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
