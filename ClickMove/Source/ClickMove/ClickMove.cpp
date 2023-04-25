5f3846205790d552516d10112743c39137a00f8a
Sho1007 <sho1007@kookmin.ac.kr>
2023-04-25 10:44:18 +0900

    Clean up Project Folder

--- ClickMove/Source/ClickMove/ClickMove.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ClickMove.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ClickMove, "ClickMove" );
--- Source/VR12ForTheKing/HexGrid/HexGridManager.cpp
+++ Source/VR12ForTheKing/HexGrid/HexGridManager.cpp
@@ -1,8 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "HexGridManager.h"
-#include "HexTile.h"
+#include "../HexGrid/HexGridManager.h"
 
 // Sets default values
 AHexGridManager::AHexGridManager()
@@ -16,26 +15,13 @@ AHexGridManager::AHexGridManager()
 void AHexGridManager::BeginPlay()
 {
 	Super::BeginPlay();
-
-	FVector SpawnLocation(0, 0, 0);
-
-	for (int i = 0; i < Height; ++i)
-	{
-		SpawnLocation.X = i % 2 == 0 ? 0.0f : XStartOffset;
-		HexGrid.Add(FTileRow());
-		for (int j = 0; j < Width; ++j)
-		{
-			AHexTile* HexTile = GetWorld()->SpawnActor<AHexTile>(HexTileClass, SpawnLocation, FRotator(0,0,0));
-			HexGrid[i].TileRowArray.Add(HexTile);
-			SpawnLocation.X += XOffset;
-		}
-		SpawnLocation.Y += YOffset;
-	}
+	
 }
 
 // Called every frame
 void AHexGridManager::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
 }
 
