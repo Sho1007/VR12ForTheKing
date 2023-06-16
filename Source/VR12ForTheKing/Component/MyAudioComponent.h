d0d955697725bb35ffa56e019b1c7943f2f6068f
serioushoon <enisy21@gmail.com>
2023-06-16 14:54:39 +0900

    AudioComponent.cpp 추가 리소스 위치 정리 오디오 테스트 맵

---
+++ Source/VR12ForTheKing/Component/MyAudioComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/AudioComponent.h"
+#include "MyAudioComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable)
+class VR12FORTHEKING_API UMyAudioComponent : public UAudioComponent
+{
+	GENERATED_BODY()
+	
+};
